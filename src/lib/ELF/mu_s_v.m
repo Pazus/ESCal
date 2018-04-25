96e68f4a90621f321b55bc65d8c2395be67a66cb
Ridzel Olga <ridzel@iap.tuwien.ac.at>
2018-04-25 09:17:43 +0200

    Test Li

--- src/lib/ELF/mu_s_v.m
+++ src/lib/ELF/mu_s_v.m
@@ -11,7 +11,7 @@ else
    end
    
    osc.eloss = w_current;
-   theta = 0:pi/2:pi/2;
+   theta = 0:pi/8:pi/2;
    phi = 0:2*pi/10:2*pi;
    
    switch(term)
@@ -25,7 +25,7 @@ else
            
            qzrcos = bsxfun(@times,qz,bsxfun(@times,r,cosd(alpha))); % qz*r*cos(alpha)
            Qcosalpha = bsxfun(@times,Q,cosd(alpha)); % Q*cos(alpha)
-           excos = bsxfun(@times,cos(qzrcos),exp(bsxfun(@times,-abs(r),Qcosalpha)));
+           excos = bsxfun(@times,cos(qzrcos),exp(bsxfun(@times,(-1)*abs(r),Qcosalpha)));
            
            weight = bsxfun(@times,bsxfun(@times,osc.qtran*a0,(sin(theta).^2)'),excos);
            qsintheta = bsxfun(@times,(osc.qtran*a0).*sqrt(2*E0/h2ev),sin(theta)');
@@ -46,60 +46,63 @@ else
            del = bsxfun(@plus,w_wave.^2,Qv_per);
            romall = bsxfun(@times,Im,bsxfun(@rdivide,repmat(weight,1, 1, length(phialpha)),del));
        case 3
-           qz = bsxfun(@times,osc.qtran*a0,cos(theta)');
-           Q = bsxfun(@times,osc.qtran*a0,sin(theta)');
+           Im = zeros(length(theta),length(osc.qtran));
+           qz = bsxfun(@times,osc.qtran.*a0,cos(theta)');
+           Q = bsxfun(@times,osc.qtran.*a0,sin(theta)');
            v_per = cosd(alpha).*sqrt(2*E0/h2ev);
            r = bsxfun(@rdivide,depth/a0,cosd(alpha));
            
            qzrcos = bsxfun(@times,qz,bsxfun(@times,r,cosd(alpha))); % qz*r*cos(alpha)
-           Qcosalpha = bsxfun(@times,Q,cosd(alpha)); % Q*cos(alpha)
-           excos = bsxfun(@times,cos(qzrcos),exp(bsxfun(@times,-abs(r),Qcosalpha)));
+           Qcosalpha = bsxfun(@times,Q,cosd(alpha)); % Q*cos(alpha)           
+           qsintheta = bsxfun(@times,osc.qtran.*a0,(sin(theta).^2)');
            
-           weight = bsxfun(@times,bsxfun(@times,osc.qtran*a0,(sin(theta).^2)'),excos);
-           qsintheta = bsxfun(@times,(osc.qtran*a0).*sqrt(2*E0/h2ev),sin(theta)');
-           phialpha = bsxfun(@times,cos(phi),sind(alpha));
+           top = bsxfun(@times,bsxfun(@times,qsintheta,cos(qzrcos)),exp(bsxfun(@times,(-1)*abs(r),Qcosalpha)));
            
-           exdim = repmat(qsintheta, 1, 1, length(phialpha)); %add extra dimension over phi
-           B = bsxfun(@times,exdim,reshape(phialpha,1,1,[]));
+           qvsintheta = bsxfun(@times,(osc.qtran*a0).*sqrt(2*E0/h2ev),sin(theta)');
+           exdim = repmat(qvsintheta, 1, 1, length(phi)); %add extra dimension over phi
+           B = bsxfun(@times,exdim,reshape(cos(phi),1,1,[]));
            
-           Qv_per = repmat(bsxfun(@times,Q.^2,v_per.^2),1, 1, length(phialpha)); %add extra dimension over phi
-           w_wave = bsxfun(@minus,osc.eloss/h2ev,B);
-           
-           osc.qtran = Q(1,:);
-           Im(1,:) = eps_sum1(osc);
-           osc.qtran = Q(2,:);
-           Im(2,:) = eps_sum1(osc);
+           w_wave = bsxfun(@minus,osc.eloss/h2ev,B.*sind(alpha));
+           Qv_per = bsxfun(@times,Q.^2,v_per.^2); %add extra dimension over phi
+           bottom = bsxfun(@plus,w_wave.^2,Qv_per);
+
+           for i = 1:length(theta)
+               osc.qtran = Q(i,:)/a0;
+               Im(i,:) = eps_sum1(osc);
+           end
            osc.qtran = a:(b-a)/2^(decdigs-1):b;
-           
-           del = bsxfun(@plus,w_wave.^2,Qv_per);
-           romall = bsxfun(@times,Im,bsxfun(@rdivide,repmat(weight,1, 1, length(phialpha)),del));
+
+           romall = bsxfun(@times,Im,bsxfun(@rdivide,top,bottom));
        case 4
-           Q = bsxfun(@times,osc.qtran*a0,sin(theta)');
+Im = zeros(length(theta),length(osc.qtran));
+           qz = bsxfun(@times,osc.qtran.*a0,cos(theta)');
+           Q = bsxfun(@times,osc.qtran.*a0,sin(theta)');
            v_per = cosd(alpha).*sqrt(2*E0/h2ev);
            r = bsxfun(@rdivide,depth/a0,cosd(alpha));
            
-           Qcosalpha = bsxfun(@times,Q,cosd(alpha)); %Q*cos(alpha)
-           excos = exp(bsxfun(@times,-abs(r),Qcosalpha));
+           Qcosalpha = bsxfun(@times,Q,cosd(alpha)); % Q*cos(alpha)           
+           qsintheta = bsxfun(@times,osc.qtran.*a0,(sin(theta).^2)');
            
-           weight = bsxfun(@times,bsxfun(@times,osc.qtran*a0,(sin(theta).^2)'),excos);
-           qsintheta = bsxfun(@times,(osc.qtran*a0).*sqrt(2*E0/h2ev),sin(theta)');
-           phialpha = bsxfun(@times,cos(phi),sind(alpha));
+           top = bsxfun(@times,qsintheta,exp(bsxfun(@times,(-1)*abs(r),Qcosalpha)));
            
-           exdim = repmat(qsintheta, 1, 1, length(phialpha)); %add extra dimension over phi
-           B = bsxfun(@times,exdim,reshape(phialpha,1,1,[]));
+           qvsintheta = bsxfun(@times,(osc.qtran*a0).*sqrt(2*E0/h2ev),sin(theta)');
+           exdim = repmat(qvsintheta, 1, 1, length(phi)); %add extra dimension over phi
+           B = bsxfun(@times,exdim,reshape(cos(phi),1,1,[]));
            
-           Qv_per = repmat(bsxfun(@times,Q.^2,v_per.^2),1, 1, length(phialpha)); %add extra dimension over phi
-           w_wave = bsxfun(@minus,osc.eloss/h2ev,B);
-           
-           del = bsxfun(@plus,w_wave.^2,Qv_per);
-           osc.qtran = Q(1,:);
-           Im(1,:) = eps_sum1(osc);
-           osc.qtran = Q(2,:);
-           Im(2,:) = eps_sum1(osc);
+           w_wave = bsxfun(@minus,osc.eloss/h2ev,B.*sind(alpha));
+           Qv_per = bsxfun(@times,Q.^2,v_per.^2); %add extra dimension over phi
+           bottom = bsxfun(@plus,w_wave.^2,Qv_per);
+
+           for i = 1:length(theta)
+               osc.qtran = Q(i,:)/a0;
+               Im(i,:) = eps_sum1(osc);
+           end
            osc.qtran = a:(b-a)/2^(decdigs-1):b;
-           temp = bsxfun(@times,Im,bsxfun(@rdivide,repmat(weight,1, 1, length(phialpha)),del));
-           newexp = exp(bsxfun(@times,-abs(r),Qcosalpha));
-           add = bsxfun(@minus,2.*cos(w_wave.*r./sqrt(2*E0/h2ev)),repmat(newexp,1, 1, length(phialpha)));
+
+           temp = bsxfun(@times,Im,bsxfun(@rdivide,top,bottom));
+           
+           newexp = exp(bsxfun(@times,(-1)*abs(r),Qcosalpha));
+           add = bsxfun(@minus,2.*cos(w_wave.*r./sqrt(2*E0/h2ev)),newexp);
            
            romall = bsxfun(@times,temp,add);
    end
--- src/lib/ELF/ndsep_Li.m
+++ src/lib/ELF/ndsep_Li.m
@@ -10,23 +10,35 @@ function diimfp = ndsep_Li(osc,E0,depth,alpha)
 %%
 
 x_in = zeros(length(osc.eloss),1);
+surfs_v_outside = zeros(length(osc.eloss),1);
+clear_bulk = zeros(length(osc.eloss),1);
+bulks_v = zeros(length(osc.eloss),1);
+surfs_v_inside = zeros(length(osc.eloss),1);
+surf_0_s_v = zeros(length(osc.eloss),1);
+
 w = osc.eloss;
 
 for i=1:length(w)-1 
     qmin = sqrt(2*E0/h2ev)-sqrt(2*(E0/h2ev-w(i)/h2ev));
     qmax = sqrt(2*E0/h2ev)+sqrt(2*(E0/h2ev-w(i)/h2ev));   
-    if (depth >= 0)
-        sum4 = mu_s_v(osc,qmin/a0,qmax/a0,17,w(i),depth,alpha,E0,4);
-        x_in(i) = 4*cosd(alpha)*sum4/pi^3;
-    else
-        sum1 = mu_s_v(osc,qmin/a0,qmax/a0,17,w(i),depth,alpha,E0,1); 
-        sum2 = mu_s_v(osc,qmin/a0,qmax/a0,17,w(i),depth,alpha,E0,2);
-        sum3 = mu_s_v(osc,qmin/a0,qmax/a0,17,w(i),depth,alpha,E0,3);
-        x_in(i) = sum1/(pi*E0) - 2*cosd(alpha)*sum2/pi^3 + 4*cosd(alpha)*sum3/pi^3;
+    if (depth > 0) % outside
+        sum4 = mu_s_v(osc,qmin/a0,qmax/a0,15,w(i),depth,alpha,E0,4);
+        surfs_v_outside(i) = 4*cosd(alpha)*sum4/pi^3/a0/h2ev;
+    elseif (depth < 0) % inside
+        %sum1 = mu_s_v(osc,qmin/a0,qmax/a0,15,w(i),depth,alpha,E0,1); 
+        %sum2 = mu_s_v(osc,qmin/a0,qmax/a0,15,w(i),depth,alpha,E0,2);
+        sum3 = mu_s_v(osc,qmin/a0,qmax/a0,15,w(i),depth,alpha,E0,3);
+        %clear_bulk(i) = sum1/(pi*E0)/a0/h2ev;
+        %bulks_v(i) = (-2)*cosd(alpha)*sum2/pi^3/a0/h2ev;
+        surfs_v_inside(i) = 4*cosd(alpha)*sum3/pi^3/a0/h2ev;
+    elseif (depth == 0)
+        sum4 = mu_s_v(osc,qmin/a0,qmax/a0,15,w(i),depth,alpha,E0,4);
+        sum3 = mu_s_v(osc,qmin/a0,qmax/a0,15,w(i),depth,alpha,E0,3);
+        surf_0_s_v(i) = 0.5*4*cosd(alpha)/pi^3/a0/h2ev*(sum4 + sum3);
     end
 end
-diimfp = x_in ./ trapz(w,x_in);
+diimfp = surfs_v_outside ./ trapz(w,surfs_v_outside);
 plot(osc.eloss,diimfp)
 hold on
-plot(osc.eloss,x_in)
+plot(osc.eloss,surfs_v_outside)
 end
